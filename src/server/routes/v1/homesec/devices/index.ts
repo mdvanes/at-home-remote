94991c2eed56287ea8c8e33d88ef7ec8756b520a
mdvanes <4253562+mdvanes@users.noreply.github.com>
2024-04-09 10:01:45 +0200

    refactor: dedupe homeSecFetch

--- src/server/routes/v1/homesec/devices/index.ts
+++ src/server/routes/v1/homesec/devices/index.ts
@@ -1,61 +1,21 @@
-import { defineEventHandler, createError } from 'h3';
+import { defineEventHandler } from 'h3';
 import type { HomesecDevicesResponse } from '../../../../../util/types';
 import { createLog } from '../../../../../util/log';
+import { homeSecFetch } from '../../../../../util/homeSecFetch';
 
 const log = createLog('v1/homesec/devices');
 
-// TODO deduplicate this "devices" route with the "toggle" webhook? Redirect/re-use?
-// http://localhost:5173/api/v1/homesec/devices
+// Dev mode: curl http://localhost:5173/api/v1/homesec/devices
+// Docker compose: curl -k https://username:password@localhost:3044/api/v1/homesec/devices
 export default defineEventHandler(
   async (event): Promise<HomesecDevicesResponse> => {
     log(event.toString());
 
-    const HOMESEC_URI = process.env['HOMESEC_URI'];
-    const HOMESEC_USER = process.env['HOMESEC_USER'];
-    const HOMESEC_PASS = process.env['HOMESEC_PASS'];
+    const response = await homeSecFetch<HomesecDevicesResponse>(
+      '/action/deviceListGet',
+      log
+    );
 
-    try {
-      const url = `${HOMESEC_URI}/action/deviceListGet`;
-
-      const headers = new Headers();
-      headers.set(
-        'Authorization',
-        'Basic ' +
-          Buffer.from(HOMESEC_USER + ':' + HOMESEC_PASS).toString('base64')
-      );
-
-      const response: HomesecDevicesResponse = await (
-        await fetch(url, { headers })
-      ).json();
-      console.log(response);
-
-      //   const response1: HomesecPanelResponse = await got(
-      //       `${this.baseUrl}/action/panelCondGet`,
-      //       {
-      //           username: this.username,
-      //           password: this.password,
-      //       }
-      //   ).json();
-      //   this.logger.log(response1);
-
-      //   const response: HomesecDevicesResponse = await got(url, {
-      //       username: this.username,
-      //       password: this.password,
-      //   }).json();
-      //   // TODO this.logger.log(response);
-      return response;
-    } catch (err) {
-      console.log(err);
-      //   this.logger.error(err);
-      //   throw new HttpException(
-      //       "failed to receive downstream data",
-      //       HttpStatus.INTERNAL_SERVER_ERROR
-      //   );
-      // throw new Error('Internal server error');
-      throw createError({
-        statusCode: 500,
-        statusMessage: 'Invalid downstream response',
-      });
-    }
+    return response;
   }
 );
--- src/server/routes/webhooks/homesec/toggle/index.ts
+++ src/server/routes/webhooks/homesec/toggle/index.ts
@@ -1,10 +1,11 @@
-import { defineEventHandler, createError } from 'h3';
+import { defineEventHandler } from 'h3';
 import { createLog } from '../../../../../util/log';
 import { challengeUserAgent } from '../../../../../util/challenge';
 import {
   PcondformModes,
   type HomesecPanelResponse,
 } from '../../../../../util/types';
+import { homeSecFetch } from '../../../../../util/homeSecFetch';
 
 const log = createLog('homesec/toggle');
 
@@ -14,66 +15,29 @@ export default defineEventHandler(
   async (event): Promise<HomesecPanelResponse> => {
     log(event.toString());
 
-    const HOMESEC_URI = process.env['HOMESEC_URI'];
-    const HOMESEC_USER = process.env['HOMESEC_USER'];
-    const HOMESEC_PASS = process.env['HOMESEC_PASS'];
-
     challengeUserAgent(log, event.node.req.headers);
 
-    try {
-      const url = `${HOMESEC_URI}/action/panelCondGet`;
+    const response = await homeSecFetch<HomesecPanelResponse>(
+      '/action/panelCondGet',
+      log
+    );
 
-      const headers = new Headers();
-      headers.set(
-        'Authorization',
-        'Basic ' +
-          Buffer.from(HOMESEC_USER + ':' + HOMESEC_PASS).toString('base64')
-      );
+    const newMode =
+      response.updates.mode_a1 !== 'Disarm'
+        ? PcondformModes.Disarm
+        : // TODO should be Arm instead of HomeArm
+          PcondformModes.HomeArm;
 
-      const response: HomesecPanelResponse = await (
-        await fetch(url, { headers })
-      ).json();
-      log(JSON.stringify(response, null, 2));
-
-      const togglePostUrl = `${HOMESEC_URI}/action/panelCondPost`;
-      const newMode =
-        response.updates.mode_a1 !== 'Disarm'
-          ? PcondformModes.Disarm
-          : // TODO should be Arm instead of HomeArm
-            PcondformModes.HomeArm;
-
-      log('newMode', newMode);
-
-      // TODO probably other response type
-      // const toggleResponse: HomesecPanelResponse = await (
-      //   await fetch(url, {
-      //     headers,
-      //     method: 'POST',
-      //     body: JSON.stringify({ area: 1, mode: newMode }),
-      //   })
-      // ).json();
-      // log(JSON.stringify(response, null, 2));
-
-      return response;
-    } catch (err) {
-      console.log(err);
-
-      throw createError({
-        statusCode: 500,
-        statusMessage: 'Invalid downstream response',
-      });
-    }
-
-    // const newState = 'Toggle';
-    // const switchType = 'switchlight';
-    // const targetUri = `${DOMOTICZ_URI}/json.htm?type=command&param=${switchType}&idx=${DOMOTICZ_SWITCH_ID}&switchcmd=${newState}`;
-    // try {
-    //   const response = await (await fetch(targetUri)).json();
-    //   log('RESPONSE', response);
-    // } catch (err) {
-    //   log('ERROR', (err as Error).message);
-    // }
-
-    // return { message: 'Toggle!' };
+    log('newMode', newMode);
+    // TODO probably other response type
+    const toggleResponse = await homeSecFetch<HomesecPanelResponse>(
+      '/action/panelCondPost',
+      log,
+      {
+        method: 'POST',
+        body: JSON.stringify({ area: 1, mode: newMode }),
+      }
+    );
+    return toggleResponse;
   }
 );
