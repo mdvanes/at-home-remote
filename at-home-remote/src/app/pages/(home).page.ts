9c03a97ab592b50f6a04dbf47292a0b55ffedf93
mdvanes <4253562+mdvanes@users.noreply.github.com>
2024-09-23 15:31:17 +0200

    use generated component

--- at-home-remote/src/app/pages/(home).page.ts
+++ at-home-remote/src/app/pages/(home).page.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { MytestComponent } from "./mytest.component";
+import { MytestComponent } from './mytest.component';
 import { AnalogWelcomeComponent } from './analog-welcome.component';
+import { ComponentsComponent } from '@at-home-remote/components';
 
 @Component({
   selector: 'at-home-remote-home',
   standalone: true,
-  imports: [AnalogWelcomeComponent, MytestComponent],
+  imports: [AnalogWelcomeComponent, MytestComponent, ComponentsComponent],
   template: `
-     <mytest ngSkipHydration />
-     <at-home-remote-analog-welcome/>
+    <lib-components></lib-components>
+    <mytest ngSkipHydration />
+    <at-home-remote-analog-welcome />
   `,
 })
-export default class HomeComponent {
-}
+export default class HomeComponent {}
