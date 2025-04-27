7e2a5ffd934f29629dd1c5fcbeb53c7669912088
mdvanes <4253562+mdvanes@users.noreply.github.com>
2025-04-27 16:39:19 +0200

    fix: homesec arm state via webhook

--- apps/at-home-remote/src/server/routes/webhooks/homesec/toggle/index.ts
+++ apps/at-home-remote/src/server/routes/webhooks/homesec/toggle/index.ts
@@ -10,7 +10,49 @@ import { homeSecFetch } from '../../../../../util/homeSecFetch';
 
 const log = createLog('homesec/toggle');
 
-// Dev mode: curl http://localhost:5173/api/webhooks/homesec/toggle
+const updateDummySensor = async (state: 'on' | 'off') => {
+  const HOMEASSISTANT_BASE_URL = process.env['HOMEASSISTANT_BASE_URL'];
+  const HOMEASSISTANT_TOKEN = process.env['HOMEASSISTANT_TOKEN'];
+  const HOMEASSISTANT_DUMMY_HOMESEC_ID =
+    process.env['HOMEASSISTANT_DUMMY_HOMESEC_ID'];
+
+  const url = `${HOMEASSISTANT_BASE_URL}/api/services/input_boolean/turn_${state}`;
+
+  try {
+    const response1 = await fetch(
+      `${HOMEASSISTANT_BASE_URL}/api/states/${HOMEASSISTANT_DUMMY_HOMESEC_ID}`,
+      {
+        headers: {
+          Authorization: `Bearer ${HOMEASSISTANT_TOKEN}`,
+        },
+      }
+    );
+    const data1 = await response1.json();
+    log('DUMMY RESPONSE', data1);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${HOMEASSISTANT_TOKEN}`,
+      },
+      body: JSON.stringify({
+        entity_id: HOMEASSISTANT_DUMMY_HOMESEC_ID,
+      }),
+    });
+
+    log('RESPONSE', url, response.status.toString(), response.statusText);
+
+    const data = await response.json();
+    log('RESPONSE DATA', data);
+
+    return {};
+  } catch (err) {
+    log('toggle error:', err as string);
+    return 'Error';
+  }
+};
+
+// Dev mode: curl http://localhost:4200/api/webhooks/homesec/toggle
 // Docker compose: curl -k https://username:password@localhost:3044/api/webhooks/homesec/toggle
 export default defineEventHandler(
   async (event): Promise<HomesecPanelPostResponse> => {
@@ -18,19 +60,20 @@ export default defineEventHandler(
 
     challengeUserAgent(log, event.node.req.headers);
 
-    const switchType = 'switchlight';
-    const DOMOTICZ_URI = process.env['DOMOTICZ_URI'];
-    const DOMOTICZ_DUMMY_HOMESEC_ID = process.env['DOMOTICZ_DUMMY_HOMESEC_ID'];
-
     const response = await homeSecFetch<HomesecPanelResponse>(
       '/action/panelCondGet',
       log
     );
 
-    const newMode =
-      response.updates.mode_a1 !== 'Disarm'
-        ? PcondformModes.Disarm
+    const HOMESEC_ARM_STATE = process.env['HOMESEC_ARM_STATE'];
+
+    const armState =
+      HOMESEC_ARM_STATE === 'HomeArm1'
+        ? PcondformModes.HomeArm1
         : PcondformModes.Arm;
+
+    const newMode =
+      response.updates.mode_a1 !== 'Disarm' ? PcondformModes.Disarm : armState;
 
     log('newMode', newMode);
 
@@ -43,15 +86,19 @@ export default defineEventHandler(
       }
     );
 
-    const newState = newMode === PcondformModes.Disarm ? 'Off' : 'On';
-    const dummyTargetUri = `${DOMOTICZ_URI}/json.htm?type=command&param=${switchType}&idx=${DOMOTICZ_DUMMY_HOMESEC_ID}&switchcmd=${newState}`;
-    try {
-      const dummyResponse = await (await fetch(dummyTargetUri)).json();
-      log('DUMMY RESPONSE', dummyResponse);
-    } catch (err) {
-      log('ERROR', (err as Error).message);
-    }
+    // To be able to send Notification from Home Assistant
+    const newState = newMode === PcondformModes.Disarm ? 'off' : 'on';
+    updateDummySensor(newState);
+
+    // const dummyTargetUri = `${DOMOTICZ_URI}/json.htm?type=command&param=${switchType}&idx=${DOMOTICZ_DUMMY_HOMESEC_ID}&switchcmd=${newState}`;
+    // try {
+    //   const dummyResponse = await (await fetch(dummyTargetUri)).json();
+    //   log('DUMMY RESPONSE', dummyResponse);
+    // } catch (err) {
+    //   log('ERROR', (err as Error).message);
+    // }
 
     return toggleResponse;
+    // return { result: 1, message: 'Updated successfully.' };
   }
 );
